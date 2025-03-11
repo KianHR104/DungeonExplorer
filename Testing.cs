14417bddf004396b24772d5d856c8eff5ce4d0f8
KianHR104 <kricketts2005@gmail.com>
2025-03-11 20:52:14 +0000

    Testing class created
    created the testing class.

---
+++ Testing.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Media;
+using System.Collections.Generic;
+
+namespace DungeonExplorer
+{
+    public class Testing
+    {
+        /// <summary>
+        /// Something testing?
+        /// </summary>
+        /// <returns> Something. </returns>
+        public void CheckSomething()
+        {
+            //return something;
+        }
+    }
+}
