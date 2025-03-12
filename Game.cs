40c4b18cc2f76d33130f4e6ae69be5f110473d2a
KianHR104 <kricketts2005@gmail.com>
2025-03-12 12:53:50 +0000

    Move between different rooms.
    The player can now move between rooms and ensures the player can't enter rooms that don't exist.

--- Game.cs
+++ Game.cs
@@ -6,16 +6,20 @@ namespace DungeonExplorer
 {
     internal class Game
     {
+        int RoomIndex = 0;
         private Player player;
-        private Room currentRoom;
+        private List<Room> CurrentRoom;
         public Game()
         {
             // Initialize the game with one room and one player.
             player = new Player("The Selected Souless", 100);
-            currentRoom = new Room("Flameconnected church", "It's an crumbling ruin situated to overlook a cliff.", 0, new List<string>{"Useless Pendant", "Zweihander", "Binoculars"});
-            SecondRoom = new Room("Souless village", "It's a decrepit, wooden housing district.", 3, new List<string>{"Residence Key", "Unending Box"});
-            ThirdRoom = new Room("Souless Communion", "It's a ruined cathedral district overrun by the Souless.", 5, new List<string>{"Titan Chunk", "Mystery Key"});
-            FourthRoom = new Room("First Chime of arising", "It's a gothic rooftop with 2 stone chimera.", 2, new List<string>{"The Chimera Halberd", "Chimera Tail Axe"});
+            CurrentRoom = new List<Room>
+            {
+            new Room("Flameconnected church", "It's an crumbling ruin situated to overlook a cliff.", 0, new List<string>{"Useless Pendant", "Zweihander", "Binoculars"}),
+            new Room("Souless village", "It's a decrepit, wooden housing district.", 3, new List<string>{"Residence Key", "Unending Box"}),
+            new Room("Souless Communion", "It's a ruined cathedral district overrun by the Souless.", 5, new List<string>{"Titan Chunk", "Mystery Key"}),
+            new Room("First Chime of arising", "It's a gothic rooftop with 2 stone chimera.", 2, new List<string>{"The Chimera Halberd", "Chimera Tail Axe"})
+            };
         }
         /// <summary>
         /// Initializes the game.
@@ -26,23 +30,23 @@ namespace DungeonExplorer
             while (playing)
             {
                 Console.Clear(); // clears screen so easier for user
-                Console.WriteLine($"You are in: {currentRoom.Name}"); // tell the user what room they are in.
+                Console.WriteLine($"You are in: {CurrentRoom[RoomIndex].Name}"); // tell the user what room they are in.
                 // Checks if enemies are in the room by seeing the enemy count, if so start a fight.
-                if (currentRoom.EnemyCount >= 1)
+                if (CurrentRoom[RoomIndex].EnemyCount >= 1)
                     {
                         Console.WriteLine("There are enemies in this room starting fight...");
                         // write code for comabt system... v
                         
                         // Ensure when player clears a room of enemies they dont respawn
-                        currentRoom.EnemyCount = 0;
+                        CurrentRoom[RoomIndex].EnemyCount = 0;
                     }
 
                 // checks if the room had an item
-                if (currentRoom.Items != null && currentRoom.Items.Count > 0)
+                if (CurrentRoom[RoomIndex].Items != null && CurrentRoom[RoomIndex].Items.Count > 0)
                     {
-                        player.PickUpItem(currentRoom.Items);
+                        player.PickUpItem(CurrentRoom[RoomIndex].Items);
                         // Empties the item incase player reloads this room
-                        currentRoom.Items.Clear();
+                        CurrentRoom[RoomIndex].Items.Clear();
                         Console.ReadKey();
                         Console.WriteLine("");
                     }
@@ -59,7 +63,14 @@ namespace DungeonExplorer
                     case "1":
                         // lets the player check room description
                         Console.Clear();
-                        Console.WriteLine(currentRoom.GetDescription());
+                        Console.WriteLine(CurrentRoom[RoomIndex].GetDescription());
+                        //Takes player to next room if there is a room available. 
+                        if (RoomIndex >= 0 && RoomIndex < CurrentRoom.Count - 1)
+                            RoomIndex += 1;
+                        else
+                            {
+                                Console.WriteLine("No more rooms.");
+                            }
                         Console.ReadKey();
                         break;
                     case "2":
--- Testing.cs
+++ Testing.cs
@@ -11,7 +11,14 @@ namespace DungeonExplorer
         /// </summary>
         public void CheckInventory()
         {
-            Debug.Assert(Items.Count > 0, "Inventory should not be empty!");
+            Debug.Assert(Items.Count > 0, "inventory should not be empty right after picking up items.");
+        }
+        /// <summary>
+        /// Checks if the RoomIndex is smaller than CurrentRoom.Count
+        /// </summary>
+        public void CheckRoomIndex()
+        {
+            Debug.Assert(RoomIndex < CurrentRoom.Count, "Room Index should be lower than the room count at all times.");
         }
     }
 }
